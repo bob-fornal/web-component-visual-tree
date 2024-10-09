d4137b8a86f20d5e7204265328bcfa45799fdead
Bob Fornal <bob.fornal@leadingedje.com>
2024-10-08 20:06:52 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-class VisualTreeElement extends HTMLElement {
-  static observedAttributes = ['parent', 'children'];
-
-  constructor() {
-    super();
-  }
-}
-
-customElements.define('visual-tree', VisualTreeElement)
